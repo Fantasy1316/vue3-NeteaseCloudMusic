a510e51e423728bd0733a0942b211011176ae0ab
Alen <1697260287@qq.com>
2022-01-04 09:30:41 +0800

    feat: 删除vconosle

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,10 +1,8 @@
 import { App } from 'vue'
 import { vantPlugins } from './vant'
 import { lazyPlugin } from './vlazy'
-import { vconsolePlugin } from './vconosle'
 
 export const setupPlugins = (app: App) => {
   app.use(vantPlugins)
   app.use(lazyPlugin)
-  app.use(vconsolePlugin)
 }
